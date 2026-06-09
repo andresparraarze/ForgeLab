78f350b92f986c3cb3e22c3ee439b454b06ea543
Andres Parra Arze <andresparraarze@gmail.com>
2026-06-09 11:06:59 -0400

    feat(formats): add glTF accessor/buffer codec

--- forgelab/formats/__init__.py
+++ forgelab/formats/__init__.py
@@ -1,5 +1,29 @@
 """Neutral file-format primitives shared by importers and exporters."""
 
+from forgelab.formats.gltf import (
+    ARRAY_BUFFER,
+    ELEMENT_ARRAY_BUFFER,
+    FLOAT,
+    UNSIGNED_INT,
+    UNSIGNED_SHORT,
+    BufferBuilder,
+    GltfError,
+    decode_accessor,
+)
 from forgelab.formats.sexpr import SExpr, SExprError, Symbol, dumps, parse
 
-__all__ = ["SExpr", "SExprError", "Symbol", "dumps", "parse"]
+__all__ = [
+    "SExpr",
+    "SExprError",
+    "Symbol",
+    "dumps",
+    "parse",
+    "ARRAY_BUFFER",
+    "ELEMENT_ARRAY_BUFFER",
+    "FLOAT",
+    "UNSIGNED_INT",
+    "UNSIGNED_SHORT",
+    "BufferBuilder",
+    "GltfError",
+    "decode_accessor",
+]
